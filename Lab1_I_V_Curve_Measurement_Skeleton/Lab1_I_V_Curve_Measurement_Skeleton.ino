a3f1492111e9bea54189b6a8c79d466672dae928
Kenyu <kenyu317@gmail.com>
2025-09-29 18:10:18 +0800

    task one but failed

--- Lab1_I_V_Curve_Measurement_Skeleton/Lab1_I_V_Curve_Measurement_Skeleton.ino
+++ Lab1_I_V_Curve_Measurement_Skeleton/Lab1_I_V_Curve_Measurement_Skeleton.ino
@@ -1,10 +1,10 @@
 #include "Arduino.h"
 
-#define Vin  //*** Define the GPIO pin 12 as Vin ***
-#define Vout   //*** Define the GPIO pin 27 as Vout ***
-//#define Cal //*** Define the GPIO pin 14 as the node for current calcultion ***
+#define Vin 12 //*** Define the GPIO pin 12 as Vin ***
+#define Vout 27  //*** Define the GPIO pin 27 as Vout ***
+// #define Cal //*** Define the GPIO pin 14 as the node for current calcultion ***
 
-//float R = ; //*** Resistance in Mohm ***
+// float R = ; //*** Resistance in Mohm ***
 
 float x;
 float y;
@@ -12,23 +12,24 @@ float y;
 
 void setup() {
 
-  Serial.begin(); //*** Define the data rate as 115200 ***
-  pinMode(Vin, ); //*** Set Vin as OUTPUT ***
-  pinMode(Vout, ); //*** Set Vout as INPUT ***
-  // pinMode(Cal, ); //*** Set Cal as INPUT ***
+  Serial.begin(115200); //*** Define the data rate as 115200 ***
+
+  pinMode(Vin, OUTPUT ); //*** Set Vin as OUTPUT ***
+  pinMode(Vout, INPUT ); //*** Set Vout as INPUT ***
+  // pinMode(Cal,INPUT ); //*** Set Cal as INPUT ***
   
-  delay();   //*** Set 100 ms delay ***
+  delay(100);   //*** Set 100 ms delay ***
   //This task only needs to be done once, thus it is placed in setup().
   Serial.println(" "); // display duty cycle,Vout for TASK 1 or I,Vout for Task 2 3 4
   for (int onPeriod = 0; onPeriod < 256; onPeriod++) 
     {
     
-    analogWrite( ,onPeriod);// Output PWM at Vin      
-    int Vout_value = analogRead(); // read the voltage level at Vout 
+    analogWrite( Vin,map(onPeriod,0,255,0,4095));// Output PWM at Vin      
+    int Vout_value = analogRead(Vout); // read the voltage level at Vout 
     // int c_value = analogRead(); // read the voltage level at the node for current calcultion  
-    // c=  ;//calculate the voltage at the node for current calcultion  
-    x =  ; // calculate duty cycle for TASK 1 or the voltage at Vout in mV for Task 2 3 4
-    y =  ; // calculate the voltage at Vout in V for TASK 1 or current in uA for Task 2 3 4
+    // c=   ;//calculate the voltage at the node for current calcultion  
+    x = (float(onPeriod)/255)*100 ; // calculate duty cycle for TASK 1 or the voltage at Vout in mV for Task 2 3 4
+    y = (float(Vout_value)*3.3)/4095  ; // calculate the voltage at Vout in V for TASK 1 or current in uA for Task 2 3 4
     Serial.print(x); 
     Serial.print(","); // seperate values by comma 
     Serial.println(y);
@@ -41,4 +42,3 @@ void loop() {
   // Code for continuous operation can be added here
  
 }
-git commit -m "Initial commit
